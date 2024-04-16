1eaa99723d8ff7dd63c82c0505cfdb971c8a75dd
Cody Carroll <cjcarroll@usfca.edu>
2024-04-16 12:08:16 -0700

    update functional prog lect

--- CodeDemos/Functions1.R
+++ CodeDemos/Functions1.R
@@ -2,6 +2,11 @@
 
 #for loops
 
+for(i in 1:5){
+  print(i^2)
+}
+
+
 for(i in 1:5) print("BSDS 100")
 
 #for the safe side, let's use brackets. This is equivalent to the above
@@ -16,22 +21,40 @@ for(i in 1:5){
   print("BSDS 100")
 }
 
+
+for(i in 1:5){
+  print(i)
+  print(i^2)
+}
+
 #we don't have to iterate numerically, instead e.g., we can go through all 
 #levels in a factor
 
 values = factor(c("A", "A", "B", "C", "C", "C", "ZzZ"))
+
+library(stringr)
+
+for(v in values){
+  print(str_c("Letter:", v))
+}
+
+
 levels(values)
+
 for(k in levels(values)){
   print(k)
 }
 
-#we can update the seq for the looping variable and it won't change the originally
-#specified seq.
+#we can update the seq for the looping variable and 
+#it won't change the originally specified seq.
 
 for(i in 1:10){
   i = i + 10
   print(i)
 }
+
+i 
+
 
 ##while loops
 #printing "beep" 3 times
@@ -39,6 +62,7 @@ n = 3
 while(n > 0){
   print("beep")
   n = n - 1
+  print(str_c("Now the value of n is: ", n, "."))
 }
 
 #contrast this with the following
@@ -46,6 +70,7 @@ n = 3
 while(n > 0){
   n = n - 1
   print("beep")
+  print(str_c("Now the value of n is: ", n, "."))
 }
 
 #what happens in the following?
@@ -54,7 +79,7 @@ while(n > 0){
   n = n + 1
   print("beep")
 }
-#crtl + c  
+#ctrl + c  
 
 #repeat loops
 # will keep executing what is inside the loop until a "break" condition is met
@@ -75,20 +100,28 @@ while(x <= 10){
 
 
 x = -3
+#if statement
+if (x < 0){
+  print("x is negative")
+}
+
+
+x = 0
 #if else statement
 if (x > 0){
   print("x is positive")
-} else{ #else must begin on the same line for which the if{} statement ends
-  print("x is negative")
+}else{ 
+  #else must begin on the same line for which the if{} statement ends
+  print("x is not positive")
 }
 
 
 #if else if statements
 
-x = 0
+#x = 0
 # x = -2
 # x = 2
-# x = "a"
+ x = "a"
 
 if(x > 0){
   print("x is positive")
@@ -111,14 +144,15 @@ if(x > 0){
 
 #vectorized if() else with ifelse()
 x = c(-1, 1)
-ifelse(x > 0, "x is positive", "x is negative")
+
+ifelse(x > 0, "x is positive", "x is not positive")
 #first argument is the condition
 #the second is the expression if condition is TRUE
 #third argument is the expression if condition is FALSE
 x = c(-1, 1, 0)
-ifelse(x > 0, "x is positive", "x is negative")
+ifelse(x > 0, "x is positive", "x is not positive")
 
-#note the above incorrectly calls 0 "negative".
+
 
 
 #to call it "x is zero" we can combine another ifelse() statement
@@ -145,13 +179,16 @@ for(i in grades){
 }
 
 #update grades according to the above switch
-grades2 = rep(0, length(grades))
-for(i in 1:length(grades)){
-  grades2[i] = switch(i,
+grades2 = list()
+for(i in grades){
+  grades2[[i]] = switch(i,
                 A = "Well Done!",
                 B = "Pretty good",
                 C = "Decent enough",
                 D = "Cutting it close...",
                 F = "RIP")
 }
+
+grades2
+
 
